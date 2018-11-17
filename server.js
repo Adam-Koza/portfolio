0c77c722eba0a18639ab2ede2ff9381addfe41d8
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-11-17 11:43:38 -0500

    Testing html file hosting.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// create express app
+// create json express app
 const app = express();
 
 // parse urlencoded requests
@@ -22,20 +22,20 @@ mongoose.connect(dbConfig.url, {
 }).then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+    console.log('Could not connect to the database.', err);
     process.exit();
 });
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to your Crypto Portfolio - track of all your coins."});
+    res.sendFile('index.html');
 });
 
 // Require Notes routes
 require('./routes/portfolio.js')(app);
 
 
-// listen for requests
+// listen for json requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
